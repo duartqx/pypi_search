36a5f29e1d94365691df391f4e5b2452312e7066
Diego Duarte <duartqx@gmail.com>
2022-04-07 15:37:30 -0300

    went back to findall since I had problems with result not found because of range(5) being too long for some searches

--- pypi_search.py
+++ pypi_search.py
@@ -4,19 +4,18 @@ Script to be used on the command line, it uses the first
 argument to search for modules on pypi websearch
 '''
 
-from re import finditer
+from re import findall
 from urllib.request import urlopen
-from typing import Callable, Iterator, Match
-from sys import argv, exit as _exit
+from sys import argv, exit as sysexit
 
 class Results:
 
-    def __init__(self, q):
+    def __init__(self, q: str) -> None:
         self.q: str = q
         self.response: str = self.get_response()
         self.results: str = self.get_results()
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return self.results
 
     def get_response(self) -> str:
@@ -24,37 +23,47 @@ class Results:
         urllib.request.urlopen to be scraped with a re.findall or re.finditer
         '''
         url_base: str = 'https://pypi.org/search/?q='
-        response: str = urlopen(url_base + self.q).read().decode('UTF-8')
+
+        response: str = ''
+
+        try:
+            response = urlopen(url_base + self.q).read().decode('UTF-8')
+        except UnicodeEncodeError:
+            pass
         return response
 
     def get_results(self) -> str:
         ''' Scrapes for name, version and description from the html received
-        with get_response() using re.finditer. Also worked with findall, but
-        since it caches all results and I want to show only the first 5
-        finditer could have better performance because it grabs the next result
-        lazily '''
+        with get_response() using re.findall. '''
+        if not self.response:
+            return '\nResult not found\n'
 
-        na: Iterator[Match[str]] = finditer('__name">*(.*)</span>', self.response)
-        ve: Iterator[Match[str]] = finditer('__version">*(.*)</span>', self.response)
-        de: Iterator[Match[str]] = finditer('__description">*(.*)</p>', self.response)
+        rsp: str = self.response
+
+        names: list[str] = findall('__name">*(.*)</span>', rsp)
+        versions: list[str] = findall('__version">*(.*)</span>', rsp)
+        descriptions: list[str] = findall('__description">*(.*)</p>', rsp)
 
         # Since re.finditer returns a callable_iterator it's required to loop
-        # through it or call next() to get the string. Calling just group()
-        # returns the full string with html tags included, and .group(1) is
-        # just the right part inside the tag
-        nxtg: Callable[[Iterator], str] = lambda i: next(i).group(1)
+        # through it or call next() to get the string. That was a problem when
+        # the results were less than the range that I was using to loop
+        # through, resulting in the exception handling to return 'Result not
+        # found'
 
-        try:
-            res: Iterator[str] = ("\n\033[1;32m{}\033[00m {}\n{}\n".format(
-                               nxtg(na), nxtg(ve), nxtg(de)) for _ in range(5))
+        if versions:
+            return ''.join("\n\033[1;32m{}\033[00m {}\n{}\n".format(
+                            names[i], versions[i], descriptions[i]) \
+                            for i in range(min(len(versions),5)))
             # The ANSI escape sequence ('\033[1;32m' + nxt(na) + '\033[00m')
             # makes the module name in the results show up green instead of the
             # default white in the terminal
-            return ''.join(res)
-        except RuntimeError:
-            # RuntimeError is raised if the generator expression res raises a
-            # StopIteration, that means finditer have not found results
+        else:
+            # if ve is empty, that means no result was found. ve is checked
+            # instead of na because na has a lot of junk from random links from
+            # pypi.org site while ve only contains version numbers found in the
+            # search
             return '\nResult not found\n'
+
 
 
 if __name__ == '__main__':
@@ -66,5 +75,5 @@ if __name__ == '__main__':
         # so that bash can know that an error ocurred
         print('\nEmpty search string.\n' + \
               'Use pip search <module> or pypi_search <module>\n')
-        _exit(1)
+        sysexit(1)
     print(Results(q))
